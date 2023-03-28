1a4b9a4d6b679b3d94cf6a83f8e84adfccf27fdd
Даня <72214499+Kilaxa@users.noreply.github.com>
2023-03-28 17:26:05 +0500

    Add files via upload

---
+++ pikpo3_python/main.py
@@ -0,0 +1,10 @@
+from processor.dataprocessor_service import DataProcessorService
+
+"""
+    Main-модуль, т.е. модуль запуска приложений ("точка входа" приложения)
+"""
+
+
+if __name__ == '__main__':
+    # Без указания полного пути, программа будет читать файл из своей корневой папки
+    DataProcessorService("Video_Performance_Over_Time.csv").run_service()
